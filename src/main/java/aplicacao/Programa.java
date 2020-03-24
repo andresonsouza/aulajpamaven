15023be24833cb2fe22ba64ef06aa5a828755a7a
andreson <a.oliveira@marquesconsult.com.br>
2020-03-23 21:42:43 -0300

    Persista os dados no banco

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -2,6 +2,7 @@ package aplicacao;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.EntityManager;
 
 import dominio.Pessoa;
 
@@ -9,15 +10,18 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Andreson", "andresonjazz@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Loide", "loideangelim@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Vagdon", "vagdon@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Andreson", "andresonjazz@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Loide", "loideangelim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Vagdon", "vagdon@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
-		
-		System.out.print(p1);
-		System.out.print(p2);
-		System.out.print(p3);
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
 	}
 
 }
