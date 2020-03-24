25bf4ab4d96bf5e9356f14ee9c8d1905666c1ead
Andreson <a.oliveira@marquesconsult.com.br>
2020-03-23 21:08:40 -0300

    Instancie o entityManagerFactory

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,5 +1,8 @@
 package aplicacao;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -9,6 +12,9 @@ public class Programa {
 		Pessoa p1 = new Pessoa(1, "Andreson", "andresonjazz@gmail.com");
 		Pessoa p2 = new Pessoa(1, "Loide", "loideangelim@gmail.com");
 		Pessoa p3 = new Pessoa(1, "Vagdon", "vagdon@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		
 		System.out.print(p1);
 		System.out.print(p2);
 		System.out.print(p3);
