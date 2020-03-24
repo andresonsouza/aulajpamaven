0a6ea658cfa682d956fc17a1452bb0474b5b4905
andreson <a.oliveira@marquesconsult.com.br>
2020-03-23 21:48:26 -0300

    Remova os objetos instanciados do projeto e feche em e emf

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,19 +9,16 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Andreson", "andresonjazz@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Loide", "loideangelim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Vagdon", "vagdon@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+
+		
+		
 		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
